422bbe237567db627929664b3fda18f369ab1ac9
theBesko <besenyeitamas.r1@gmail.com>
2021-06-27 18:20:57 +0200

    bra

--- pages/index.js
+++ pages/index.js
@@ -1,4 +1,7 @@
 export default function Home() {
-  return ( <h1>halo</h1>
-  )
-}
+  return (
+    <>
+      <h1>halo</h1>
+    </>
+  );
+};
--- pages/uj.js
+++ pages/uj.js
@@ -1,3 +1,18 @@
+import { Fragment } from "react";
+import Image from "next/image";
+
 export default function Home() {
-  return <h1>masikoldal</h1>;
+  return (
+    <Fragment>
+      <h1>masikoldal</h1>
+      <div style={{backgroundColor: "green"}}>
+        <Image
+          src="https://www.wwe.com/f/styles/wwe_1_1_540/public/rd-talent/Profile/Roman_Reigns_pro.png"
+          width="540"
+          height="640"
+          layout="responsive"
+        />
+      </div>
+    </Fragment>
+  );
 }
